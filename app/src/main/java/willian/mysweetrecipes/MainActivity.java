27c7140d3f30e6d70901b1a39f192b5d4901dff0
Willian Duque <duquewillian@hotmail.com>
2017-07-22 21:11:32 -0300

    handles connectivity issues when loading data from the api

--- app/src/main/java/willian/mysweetrecipes/MainActivity.java
+++ app/src/main/java/willian/mysweetrecipes/MainActivity.java
@@ -4,9 +4,20 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import willian.mysweetrecipes.model.Recipe;
+import willian.mysweetrecipes.rest.ApiService;
 import willian.mysweetrecipes.rest.RetroClient;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,7 +25,9 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     @BindView(R.id.recyclerview_recipes)
     RecyclerView mRecyclerView;
-    private RecipesAdapter mRecipesAdapter;
+    @BindView(R.id.pbProgress)
+    ProgressBar mLoadingIndicator;
+    private static RecipesAdapter mRecipesAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         mRecipesAdapter = new RecipesAdapter();
 
-        RetroClient.getApiData(mRecipesAdapter);
+        getApiData();
 
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setLayoutManager(
@@ -31,5 +44,47 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView.setAdapter(mRecipesAdapter);
     }
 
+    /**
+     * GET data from the API and load it on the Adapter
+     */
+    public void getApiData() {
+        ApiService api = RetroClient.getApiService();
+        Call<List<Recipe>> call = api.getRecipes();
+
+        call.enqueue(new Callback<List<Recipe>>() {
+            @Override
+            public void onResponse(Call<List<Recipe>> call, Response<List<Recipe>> response) {
+                if(!response.isSuccessful() ){
+                    showErrorMessage();
+                    return;
+                }
+                List<Recipe> recipes = response.body();
+                mRecipesAdapter.setRecipeList(recipes);
+                mRecipesAdapter.notifyDataSetChanged();
+                showRecipes();
+            }
+
+            @Override
+            public void onFailure(Call<List<Recipe>> call, Throwable t) {
+                showErrorMessage();
+
+                if (t.getMessage().equals("Unable to resolve host \"go.udacity.com\": No address associated with hostname")) {
+                    Log.e(TAG, "There is no Internet connection or the API is down");
+                } else {
+                    Log.e(TAG, t.getMessage());
+                }
+            }
+        });
+    }
+
+    private void showErrorMessage() {
+        mLoadingIndicator.setVisibility(View.GONE);
+        Toast.makeText(this,"Unable to sync with the server. Please check your connection and try again",Toast.LENGTH_LONG).show();
+    }
+
+    private void showRecipes() {
+        mLoadingIndicator.setVisibility(View.GONE);
+        mRecyclerView.setVisibility(View.VISIBLE);
+    }
 
 }
--- app/src/main/java/willian/mysweetrecipes/rest/RetroClient.java
+++ app/src/main/java/willian/mysweetrecipes/rest/RetroClient.java
@@ -1,16 +1,7 @@
 package willian.mysweetrecipes.rest;
 
-import android.util.Log;
-
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
-import willian.mysweetrecipes.RecipesAdapter;
-import willian.mysweetrecipes.model.Recipe;
 
 public class RetroClient {
 
@@ -32,37 +23,8 @@ public class RetroClient {
      *
      * @return API Service
      */
-    private static ApiService getApiService() {
+    public static ApiService getApiService() {
         return getRetrofitInstance().create(ApiService.class);
     }
 
-    /**
-     * GET data from the API and load it on the Adapter
-     *
-     * @param adapter the adapter that will receive the data
-     */
-    public static void getApiData(final RecipesAdapter adapter) {
-        //Creating an object of the apiService interface
-        ApiService api = RetroClient.getApiService();
-
-        Call<List<Recipe>> call = api.getRecipes();
-
-        call.enqueue(new Callback<List<Recipe>>() {
-            @Override
-            public void onResponse(Call<List<Recipe>> call, Response<List<Recipe>> response) {
-                List<Recipe> recipes = response.body();
-                adapter.setRecipeList(recipes);
-                adapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onFailure(Call<List<Recipe>> call, Throwable t) {
-                if (t.getMessage().equals("Unable to resolve host \"go.udacity.com\": No address associated with hostname")) {
-                    Log.e(TAG, "There is no Internet connection or the API is down");
-                } else {
-                    Log.e(TAG, t.getMessage());
-                }
-            }
-        });
-    }
 }
